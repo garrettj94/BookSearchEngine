29e6ada03b621561cda5a761710a4448858592fd
Garrett Jackson <garrettjackson43@gmail.com>
2022-12-14 12:57:41 -0500

    moved server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const db = require('./server/config/connection');
+const routes = require('./server/routes');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./server/schemas');
 
 
 const app = express();
